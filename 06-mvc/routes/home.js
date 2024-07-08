f8a464634762c7adbffc3d186fb669dad02ab4b7
alasaidi <alasaidi55gmail.com>
2024-07-08 20:14:21 +0200

    mvc module

--- 06-mvc/routes/home.js
+++ 06-mvc/routes/home.js
@@ -4,8 +4,13 @@ import homeControllers from '../controllers/home.js';
 
 const router = express.Router();
 
+// add new book
+router.post('/add', homeControllers.addBook);
+
 // get books
 router.get('/', homeControllers.getBooks);
 router.get('/:id', homeControllers.getBook);
 
+// Delete books
+router.delete('/:id', homeControllers.deleteBook);
 export default router;
